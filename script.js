258fb8167e2d59c94c69818a02f288c0518f765b
aprillebalsom <aprillebalsom@gmail.com>
2020-11-01 21:57:36 -0500

    added styling

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const bdayapp = {};
 
 bdayapp.displayPhoto = function () {
-
+  
     // on click, hide the button and show the card
     const btn = document.getElementById('main-btn');
 
     btn.addEventListener('click', () => {
+
+        //remove the height on the first section
+        const first = document.getElementById('first-page');
+        first.style.height = "0px";
+
+        //hide the button
         btn.classList.add('hide');
 
         const second = document.getElementById('second-page');
@@ -13,8 +19,22 @@ bdayapp.displayPhoto = function () {
     });
 }
 
+bdayapp.displayMsg = function () {
+
+    // on click, hide the button and show the card
+    const btn = document.getElementById('second-btn');
+
+    btn.addEventListener('click', () => {
+        
+        const third = document.getElementById('third-page');
+        third.classList.toggle('hide');
+    });
+}
+
+// function that kicks off the app
 bdayapp.init = function () {
     bdayapp.displayPhoto();
+    bdayapp.displayMsg();
 }
 
 // document ready
