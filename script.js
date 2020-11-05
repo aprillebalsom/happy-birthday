59f73acb43d69621008522dc8e7fe726b3c4e564
aprillebalsom <aprillebalsom@gmail.com>
2020-11-05 11:29:21 -0500

    added assets

--- script.js
+++ script.js
@@ -19,22 +19,70 @@ bdayapp.displayPhoto = function () {
     });
 }
 
+bdayapp.selectPartyItems = function () {
+    // const hatBtn = document.getElementById('hat');
+    // const catBtn = document.getElementById('cat');
+    // const surpriseBtn = document.getElementById('surprise');
+
+    const btns = document.querySelectorAll('.party-btn');
+
+    btns.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+           const btnId = event.target.id;
+
+            if (btnId === 'hat'){
+                bdayapp.randomizePartyItems();
+
+            } else if (btnId === 'cat') {
+                bdayapp.randomizePartyItems();
+
+            } else if (btnId === 'surprise') {
+                bdayapp.randomizePartyItems();
+
+            }
+        });
+    })
+
+
+}
+
+bdayapp.randomizePartyItems = function () {
+
+    
+
+}
+
+
+bdayapp.displayPartyItems = function () {
+
+}
+
 bdayapp.displayMsg = function () {
 
-    // on click, hide the button and show the card
-    const btn = document.getElementById('second-btn');
 
+    const btn = document.getElementById('second-btn');
+    const third = document.getElementById('third-page');
+    const exitBtn = document.getElementById('exit');
+
+    // on click, hide the button and show the card
     btn.addEventListener('click', () => {
-        
-        const third = document.getElementById('third-page');
+    
         third.classList.toggle('hide');
     });
+
+    // on click, hide the card again
+    exitBtn.addEventListener('click', () => {
+
+        third.classList.toggle('hide');
+    });
+
 }
 
 // function that kicks off the app
 bdayapp.init = function () {
     bdayapp.displayPhoto();
     bdayapp.displayMsg();
+    bdayapp.selectPartyItems();
 }
 
 // document ready
